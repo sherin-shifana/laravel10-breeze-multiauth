b56ce0b2a3e269c6551fb019e3df237a3cb9c3a6
Sherin Shifana <sherin@bourgeontech.com>
2023-05-20 18:22:14 +0530

    first commit

---
+++ routes/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Admin\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => ['auth:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
